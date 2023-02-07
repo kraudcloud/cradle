827302a9d30182cfe3119458ceb3b3e46a44a4e8
Arvid E. Picciani <arvid@kraud.cloud>
2023-02-07 13:09:38 +0100

    update snp firmware

--- deploy-snp.sh
+++ deploy-snp.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+ssh yca7h rm -rf /opt/kraud/cradle/pkg.old
+ssh yca7h mv /opt/kraud/cradle/pkg /opt/kraud/cradle/pkg.old
 rsync -avz ./ yca7h:/opt/kraud/cradle/ --exclude /build --exclude /test
